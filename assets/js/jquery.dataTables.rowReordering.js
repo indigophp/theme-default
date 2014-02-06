db873f278b45bea69083a41318a10c2ad18a0e51
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-02-06 15:28:30 +0100

    Added jQuery and Migrate

--- assets/js/jquery.dataTables.rowReordering.js
+++ assets/js/jquery.dataTables.rowReordering.js
@@ -19,7 +19,7 @@
 */
 (function ($) {
 
-        "use strict";
+    "use strict";
     $.fn.rowReordering = function (options) {
 
         function _fnStartProcessingMode(oTable) {
@@ -42,46 +42,37 @@
             }
         }
 
-        ///Not used
-        function fnGetStartPosition(oTable, sSelector) {
-            var iStart = 1000000;
-            $(sSelector, oTable).each(function () {
-                iPosition = parseInt(oTable.fnGetData(this, properties.iIndexColumn));
-                if (iPosition < iStart)
-                    iStart = iPosition;
-            });
-            return iStart;
-        }
-
         function fnCancelSorting(oTable, tbody, properties, iLogLevel, sMessage) {
             tbody.sortable('cancel');
-            if(iLogLevel<=properties.iLogLevel){
-                    if(sMessage!= undefined){
+            if(iLogLevel <= properties.iLogLevel){
+                    if(sMessage != undefined) {
                             properties.fnAlert(sMessage, "");
-                    }else{
+                    } else{
                             properties.fnAlert("Row cannot be moved", "");
                     }
             }
+
             properties.fnEndProcessingMode(oTable);
         }
 
         function fnGetState(oTable, sSelector, id) {
             var tr = $("#" + id, oTable);
-            var iCurrentPosition = oTable.fnGetData(tr[0], properties.iIndexColumn);
-            var iNewPosition = -1; // fnGetStartPosition(sSelector);
+            var iCurrentPosition = fnGetPosition(oTable, tr);
+
+            var iNewPosition = -1;
             var sDirection;
             var trPrevious = tr.prev(sSelector);
             if (trPrevious.length > 0) {
-                iNewPosition = parseInt(oTable.fnGetData(trPrevious[0], properties.iIndexColumn));
+                iNewPosition = fnGetPosition(oTable, trPrevious);
                 if (iNewPosition < iCurrentPosition) {
-                    iNewPosition = iNewPosition + 1;
+                    iNewPosition = iNewPosition + properties.iStep;
                 }
             } else {
                 var trNext = tr.next(sSelector);
                 if (trNext.length > 0) {
-                    iNewPosition = parseInt(oTable.fnGetData(trNext[0], properties.iIndexColumn));
+                    iNewPosition = fnGetPosition(oTable, trNext);
                     if (iNewPosition > iCurrentPosition)//moved back
-                        iNewPosition = iNewPosition - 1;
+                        iNewPosition = iNewPosition - properties.iStep;
                 }
             }
             if (iNewPosition < iCurrentPosition)
@@ -90,7 +81,31 @@
                 sDirection = "forward";
 
             return { sDirection: sDirection, iCurrentPosition: iCurrentPosition, iNewPosition: iNewPosition };
+        }
 
+        function fnGetPosition(oTable, tr) {
+            if (typeof properties.iIndexColumn === "number") {
+                var position = oTable.fnGetData(tr[0], properties.iIndexColumn);
+            } else {
+                var position = tr.data(properties.iIndexColumn);
+            }
+
+            return parseInt(position);
+        }
+
+        function fnSetPosition(oTable, tr, position) {
+            if (typeof properties.iIndexColumn === "number") {
+                oTable.fnUpdate(
+                    position,
+                    oTable.fnGetPosition(tr[0]), // get row position in current model
+                    properties.iIndexColumn,
+                    false
+                ); // false = defer redraw until all row updates are done
+            } else {
+                tr.data(properties.iIndexColumn, position);
+            }
+
+            return parseInt(position);
         }
 
         function fnMoveRows(oTable, sSelector, iCurrentPosition, iNewPosition, sDirection, id, sGroup) {
@@ -104,50 +119,38 @@
             $(oTable.fnGetNodes()).each(function () {
                 if (sGroup != "" && $(this).attr("data-group") != sGroup)
                     return;
-                var tr = this;
-                var iRowPosition = parseInt(oTable.fnGetData(tr, properties.iIndexColumn));
+                var tr = $(this);
+                var iRowPosition = fnGetPosition(oTable, tr);
                 if (iStart <= iRowPosition && iRowPosition <= iEnd) {
-                    if (tr.id == id) {
-                        oTable.fnUpdate(
-                            iNewPosition,
-                            oTable.fnGetPosition(tr), // get row position in current model
-                            properties.iIndexColumn,
-                            false
-                        ); // false = defer redraw until all row updates are done
+                    if (tr[0].id == id) {
+                        fnSetPosition(oTable, tr, iNewPosition);
                     } else {
                         if (sDirection == "back") {
-                            oTable.fnUpdate(
-                                iRowPosition + properties.iStep,
-                                oTable.fnGetPosition(tr), // get row position in current model
-                                properties.iIndexColumn,
-                                false
-                            ); // false = defer redraw until all row updates are done
+                            fnSetPosition(oTable, tr, iRowPosition + properties.iStep);
                         } else {
-                            oTable.fnUpdate(
-                                iRowPosition - properties.iStep,
-                                oTable.fnGetPosition(tr), // get row position in current model
-                                properties.iIndexColumn,
-                                false
-                            ); // false = defer redraw until all row updates are done
+                            fnSetPosition(oTable, tr, iRowPosition - properties.iStep);
                         }
                     }
                 }
             });
 
-            var oSettings = oTable.fnSettings();
+            if (typeof properties.iIndexColumn == "number") {
+                var oSettings = oTable.fnSettings();
 
-            //Standing Redraw Extension
-            //Author:   Jonathan Hoguet
-            //http://datatables.net/plug-ins/api#fnStandingRedraw
-            if (oSettings.oFeatures.bServerSide === false) {
-                var before = oSettings._iDisplayStart;
-                oSettings.oApi._fnReDraw(oSettings);
-                //iDisplayStart has been reset to zero - so lets change it back
-                oSettings._iDisplayStart = before;
-                oSettings.oApi._fnCalculateEnd(oSettings);
+                //Standing Redraw Extension
+                //Author:   Jonathan Hoguet
+                //http://datatables.net/plug-ins/api#fnStandingRedraw
+                if (oSettings.oFeatures.bServerSide === false) {
+                    var before = oSettings._iDisplayStart;
+                    oSettings.oApi._fnReDraw(oSettings);
+                    //iDisplayStart has been reset to zero - so lets change it back
+                    oSettings._iDisplayStart = before;
+                    oSettings.oApi._fnCalculateEnd(oSettings);
+                }
+                //draw the 'current' page
+                oSettings.oApi._fnDraw(oSettings);
             }
-            //draw the 'current' page
-            oSettings.oApi._fnDraw(oSettings);
+
         }
 
         function _fnAlert(message, type) { alert(message); }
@@ -189,21 +192,18 @@
 
             var oTable = $(this).dataTable();
 
-            var aaSortingFixed = (oTable.fnSettings().aaSortingFixed == null ? new Array() : oTable.fnSettings().aaSortingFixed);
-            aaSortingFixed.push([properties.iIndexColumn, "asc"]);
+            if (typeof properties.iIndexColumn == "number") {
+                var aaSortingFixed = (oTable.fnSettings().aaSortingFixed == null ? new Array() : oTable.fnSettings().aaSortingFixed);
+                aaSortingFixed.push([properties.iIndexColumn, "asc"]);
 
-            oTable.fnSettings().aaSortingFixed = aaSortingFixed;
+                oTable.fnSettings().aaSortingFixed = aaSortingFixed;
 
+                for (var i = 0; i < oTable.fnSettings().aoColumns.length; i++) {
+                    oTable.fnSettings().aoColumns[i].bSortable = false;
+                }
 
-            for (var i = 0; i < oTable.fnSettings().aoColumns.length; i++) {
-                oTable.fnSettings().aoColumns[i].bSortable = false;
-                /*for(var j=0; j<aaSortingFixed.length; j++)
-                {
-                if( i == aaSortingFixed[j][0] )
-                oTable.fnSettings().aoColumns[i].bSortable = false;
-                }*/
+                oTable.fnDraw();
             }
-            oTable.fnDraw();
 
             $("tbody", oTable).disableSelection().sortable({
                 cursor: "move",
@@ -215,30 +215,32 @@
                     var sGroup = "";
                     if (properties.bGroupingUsed) {
                         sGroup = $(ui.item).attr(properties.sDataGroupAttribute);
-                    if(sGroup==null || sGroup==undefined){
-                       fnCancelSorting($dataTable, tbody, properties, 3, "Grouping row cannot be moved");
-                       return;
-                    }
+                        if(sGroup==null || sGroup==undefined){
+                           fnCancelSorting($dataTable, tbody, properties, 3, "Grouping row cannot be moved");
+                           return;
+                        }
                         sSelector = "tbody tr[" + properties.sDataGroupAttribute + " ='" + sGroup + "']";
                     }
 
                     var oState = fnGetState($dataTable, sSelector, ui.item.context.id);
                     if(oState.iNewPosition == -1)
                     {
-                       fnCancelSorting($dataTable, tbody, properties,2);
+                       fnCancelSorting($dataTable, tbody, properties, 2);
                        return;
                     }
 
                     if (properties.sURL != null) {
                         properties.fnStartProcessingMode($dataTable);
-                                                var oAjaxRequest = {
+                        var oAjaxRequest = {
                             url: properties.sURL,
                             type: properties.sRequestType,
-                            data: { id: ui.item.context.id,
-                                fromPosition: oState.iCurrentPosition,
-                                toPosition: oState.iNewPosition,
-                                direction: oState.sDirection,
-                                group: sGroup
+                            data: {
+                                "id": ui.item.context.id,
+                                "fromPosition": oState.iCurrentPosition,
+                                "toPosition": oState.iNewPosition,
+                                "step": properties.iStep,
+                                "direction": oState.sDirection,
+                                "group": sGroup
                             },
                             success: function (data) {
                                 properties.fnSuccess(data);
@@ -249,7 +251,7 @@
                                 fnCancelSorting($dataTable, tbody, properties, 1, jqXHR.statusText);
                             }
                         };
-                                                properties.fnUpdateAjaxRequest(oAjaxRequest, properties, $dataTable);
+                        properties.fnUpdateAjaxRequest(oAjaxRequest, properties, $dataTable);
                         $.ajax(oAjaxRequest);
                     } else {
                         fnMoveRows($dataTable, sSelector, oState.iCurrentPosition, oState.iNewPosition, oState.sDirection, ui.item.context.id, sGroup);
