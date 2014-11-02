468e8dc301a01960486fd0c93c644dd319666128
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-11-02 12:43:59 +0100

    Adds service provider for twig

---
+++ FuelServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the FuelPHP Menu package.
+ *
+ * (c) Indigo Development Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Indigo\Theme\Default\Providers;
+
+use Fuel\Dependency\ServiceProvider;
+
+/**
+ * Provides twig services
+ *
+ * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ */
+class FuelServiceProvider extends ServiceProvider
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public $provides = true;
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function provide()
+	{
+		// menu extension
+		$this->extend('parser.twig', function($dic, $instance)
+		{
+			$rendererProvider = $dic->resolve('menu.renderer_provider');
+			$menuProvider = $dic->resolve('menu.provider');
+
+			$helper = $dic->resolve('Knp\\Menu\\Twig\\Helper', [$rendererProvider, $menuProvider]);
+
+			$extension = $dic->resolve('Knp\\Menu\\Twig\\MenuExtension', [$helper]);
+
+			$twig = $instance->getTwig();
+
+			$twig->addExtension($extension);
+
+			return $instance;
+		});
+	}
+}
